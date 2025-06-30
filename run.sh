9e37e563cd359ea2ba28af888e7f9287683af793
Rishabh Singh <rishabh.si@browserstack.com@Rishabhs-MacBook-Air.local>
2025-06-30 14:02:27 +0530

    update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ git add .
 git commit -m "update"
 git push
 jenkins-lts-cli -s http://localhost:8080/ -auth rish:777111 build pipeline-Tuts-1  
-sleep(2)
+sleep 2
 jenkins-lts-cli -s http://localhost:8080/ -auth rish:777111 console pipeline-Tuts-1  
