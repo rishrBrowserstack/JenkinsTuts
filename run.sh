95373cc00ac44c5d211723ec60073fd24d6385fc
Rishabh Singh <rishabh.si@browserstack.com@Rishabhs-MacBook-Air.local>
2025-06-30 14:18:05 +0530

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "update"
 git push
-jenkins-lts-cli -s http://localhost:8080/ -auth rish:777111 build pipeline-Tuts-1   -p USERNAME=rish -p ENVIRONMENT=macAirLocal -p DEBUG=true
+jenkins-lts-cli -s http://localhost:8080/ -auth rish:777111 build pipeline-Tuts-1   -p USERNAME=rish -p ENVIRONMENT=qa -p DEBUG=true
 # sleep 10
 # jenkins-lts-cli -s http://localhost:8080/ -auth rish:777111 console pipeline-Tuts-1   >> jenkins.out.console
